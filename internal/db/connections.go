1c4e3719cae924992310dc1bd0578ef85d4522d0
astianmuchui <astianmuchui@gmail.com>
2025-10-20 18:46:29 +0300

    Fix: CGO Errors on SQLITE

--- internal/db/connections.go
+++ internal/db/connections.go
@@ -3,7 +3,7 @@ package db
 import (
 	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
+	sqlite "github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 
 	"github.com/astianmuchui/nexthings-core/internal/env"
@@ -13,38 +13,35 @@ import (
 var DatabaseName string
 var WorkingEnv string
 
+var DB *gorm.DB
+
 func init() {
 	env.Load()
 	WorkingEnv = env.GetWorkingEnvironment()
 }
 
-func Connect() *gorm.DB  {
-	var db *gorm.DB
-	if WorkingEnv == env.LOCAL {
-		// Use sqlite
-
-		db, connErr := gorm.Open(sqlite.Open("nexthings.sqlite"), &gorm.Config{})
-
-		if connErr != nil {
-			log.Error("Failed to connect to database: ", connErr)
-		}
-		return db
-	} else if WorkingEnv == env.PRODUCTION {
+func Connect() {
+	if WorkingEnv == env.PRODUCTION {
 		// Use Postgres Connection
-
 		dsn, dsnErr := env.GetDatabaseDSN()
 
 		if dsnErr != nil {
 			log.Error("Unable to get DSN")
 		}
 
-		db, connError := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-		if connError!= nil {
-			log.Error("Unable to get DSN")
+		if err != nil {
+			log.Error("Unable to connect to Postgres database: ", err)
 		}
 
-		return db
+	} else {
+		var err error
+		DB, err = gorm.Open(sqlite.Open("nexthings.sqlite"), &gorm.Config{})
+
+		if err != nil {
+			log.Error("Failed to connect to SQLite database: ", err)
+		}
 	}
-	return db
 }
