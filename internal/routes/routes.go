0fd6dc2d675f123f3496d7f5c1e476a1d9e7b034
astianmuchui <astianmuchui@gmail.com>
2025-10-28 23:20:00 +0300

    Router

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,10 +3,17 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/astianmuchui/nexthings-core/internal/handlers/api"
 	"github.com/astianmuchui/nexthings-core/internal/handlers"
 
 )
 
 func GetRoutes(app *fiber.App) {
 	app.Get("/", handlers.HomeHandler)
+
+	app.Route("/api/v1", func (v1 fiber.Router)  {
+		v1.Route("/users", func (users fiber.Router) {
+			users.Post("/register", api.UserApiRegisterHandler)
+		})
+	})
 }
