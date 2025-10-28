3508cf8da52ad3b51503c7888010cc70310faacf
astianmuchui <astianmuchui@gmail.com>
2025-10-28 23:19:48 +0300

    Updated user model and crud functionalities

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,24 +1,29 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 
 	"github.com/astianmuchui/nexthings-core/internal/db"
 
 )
+
 type User struct {
 	BaseModel
-	FirstName string    `json:"firstname"`
-	LastName  string    `json:"lastname"`
-	Username string `json:"username" gorm:"uniqueIndex;not null"`
-	Email string `json:"email" gorm:"uniqueIndex;not null"`
+	FirstName       string    `json:"firstname"`
+	LastName        string    `json:"lastname"`
+	Username        string    `json:"username" gorm:"uniqueIndex;not null"`
+	Email           string    `json:"email" gorm:"uniqueIndex;not null"`
 
-	PhoneNumber string `json:"phone" gorm:"uniqueIndex;not null"`
-	City string `json:"city"`
-	Country string `json:"country"`
+	PhoneNumber     string    `json:"phone" gorm:"uniqueIndex;not null"`
+	City            string    `json:"city"`
+	Country         string    `json:"country"`
+
+	EmailVerifiedAt time.Time `json:"EmailVerifiedAt"`
+	PhoneVerifiedAt time.Time `json:"PhoneVerifiedAt"`
 }
-
 
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
@@ -34,6 +39,12 @@ func (u *User) Create() (err error) {
 
 func (u *User) Update() (err error) {
 	result := db.DB.Save(u)
+
+	return result.Error
+}
+
+func (u *User) Retreive() error {
+	result := db.DB.Model(User{}).Where("email = ? or uuid = ? or username = ? or phone_number = ?", u.Email, u.Uuid, u.Username, u.PhoneNumber).First(&u)
 
 	return result.Error
 }
