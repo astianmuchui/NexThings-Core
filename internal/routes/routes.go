2810956914f3489e79817e3d9e46c8abd661a135
astianmuchui <astianmuchui@gmail.com>
2025-10-29 18:23:52 +0300

    User Auth APIs

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,19 +1,27 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2"
 
-	"github.com/astianmuchui/nexthings-core/internal/handlers/api"
-	"github.com/astianmuchui/nexthings-core/internal/handlers"
-
+    "github.com/astianmuchui/nexthings-core/internal/handlers/api"
+    "github.com/astianmuchui/nexthings-core/internal/handlers"
 )
 
 func GetRoutes(app *fiber.App) {
-	app.Get("/", handlers.HomeHandler)
 
-	app.Route("/api/v1", func (v1 fiber.Router)  {
-		v1.Route("/users", func (users fiber.Router) {
-			users.Post("/register", api.UserApiRegisterHandler)
-		})
-	})
+    app.Get("/", handlers.HomeHandler)
+
+    app.Route("/api/v1", func(v1 fiber.Router) {
+
+        v1.Route("/users", func(users fiber.Router) {
+
+            users.Post("/register", api.UserApiRegisterHandler)
+
+            users.Post("/login", api.UserApiLoginHandler)
+            users.Get("/verify-account/:uid/:token", api.UserApiVerifyAccountHandler)
+            users.Patch("/reset-password/", api.UserApiResetPasswordHandler)
+
+        })
+
+    })
 }
